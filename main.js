6c67f3a1735692d5c27afe6023655a7a925f86c9
Krokodili <luga.geri@gmail.com>
2021-11-17 15:09:52 +0100

    Finshed challenge 2

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-for(let i=1;i<101;i++){
-    i%3===0 && i%5===0 && i%7===0 && i%8===0?console.log('FizzBushFishBus'):
-    i%3===0 && i%5===0 && i%7===0?console.log('FizzBuzzFish'):
-    i%3===0 && i%5===0? console.log('FizzBuzz'):
-    i%3===0?console.log('Fizz'):
-    i%5===0?console.log('Buzz'):
-    // i%7===0?console.log('Fish'):
-    // i%8===0?console.log('Bus'):
-    console.log(i)
+// for(let i=1;i<101;i++){
+//     i%3===0 && i%5===0 && i%7===0 && i%8===0?console.log(`${i} FizzBushFishBus`):
+//     i%3===0 && i%5===0 && i%7===0?console.log('FizzBuzzFish'):
+//     i%3===0 && i%5===0? console.log('FizzBuzz'):
+//     i%3===0?console.log('Fizz'):
+//     i%5===0?console.log('Buzz'):
+//     console.log(i)
+// }
 
-}
+
+
+// Challenge 2 below
+
+
+
+// for(let i = 1;;i++){
+//     if(i%3===0 && i%5===0 && i%7===0 && i%8===0){
+//     console.log(`${i} is first number to print FizzBushFishBus`)
+//     break;}
+// }
+
+
